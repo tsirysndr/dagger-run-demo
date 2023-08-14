6a73f0902695a10493ca0a08a7dffa95f88eb793
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-14 09:49:28 +0000

    update test

--- main_test.ts
+++ main_test.ts
@@ -2,5 +2,5 @@ import { assertEquals } from "https://deno.land/std@0.197.0/assert/mod.ts";
 import { add } from "./main.ts";
 
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+  assertEquals(add(2, 3), 6);
 });
