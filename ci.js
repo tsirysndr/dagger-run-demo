0542ffafbd0a1aef59764f112d559cca4abed4d7
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-14 09:38:08 +0000

    update ci

--- ci.js
+++ ci.js
@@ -1,13 +1,19 @@
 import { connect } from "https://esm.sh/@dagger.io/dagger";
 
 connect(async (client) => {
-  const ctr = client.container().from("oven/bun")
-	.withDirectory("/app", client.host().directory(".", { exclude: [ "node_modules/", ".git" ]  }))
-	.withWorkdir("/app")
-	.withExec(["bun", "install"])
-	.withExec(["bun", "test"]);
+  const ctr = client
+    .container()
+    .from("oven/bun")
+    .withDirectory(
+      "/app",
+      client.host().directory(".", { exclude: ["node_modules/", ".git"] })
+    )
+    .withWorkdir("/app")
+    .withExec(["bun", "install"])
+    .withExec(["bun", "test"]);
+
+  await ctr.file("/app/calc.test.js").export("./calc.test.new.js");
 
   console.log(await ctr.stdout());
   console.log(await ctr.stderr());
 });
-
